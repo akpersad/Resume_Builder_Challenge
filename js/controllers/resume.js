8a06f757122a49041820d1026d35f78411bf7961
akpersad <akpersad@gmail.com>
2016-11-27 20:12:58 -0500

    Figured out how to add text to specific div

--- js/controllers/resume.js
+++ js/controllers/resume.js
@@ -7,9 +7,54 @@
 
     function ResumeController(DataService) {
         this.DataService = DataService;
-        this.text = DataService.processFile;
-        debugger;
+        this.upload = upload;
+
+        var fileInput = $('#files');
+
+        function upload() {
+            console.log("hello");
+            changeIt(true);
+        }
+
+        function changeIt(state) {
+            DataService.activate(state);
+        }
+
+        $('#upload').on('click', function() {
+            if(!window.FileReader) {
+                alert('Your browser is not supported')
+            }
+            var input = fileInput.get(0);
+
+            // Create a reader object
+            var reader = new FileReader();
+            // debugger;
+            if(input.files.length) {
+                if(input.files[0].type === "text/plain") {
+                    var textFile = input.files[0];
+                    reader.readAsText(textFile);
+                    $(reader).on('load', processFile);
+                } else {
+                    changeIt(false);
+                    alert('Please upload a .txt file')
+                }
+            } else {
+                changeIt(false);
+                alert('Please upload a file before continuing')
+            }
+        });
+
+        function processFile(e) {
+            var file = e.target.result,
+                results;
+                // debugger;
+            if(file && file.length) {
+                var div = document.getElementById('TextArea');
+                results = DataService.dataParse(file);
+                div.innerHTML += results;
+                // debugger;
+            }
+            // return results;
+        }
     }
-
-
 })();
--- js/factories/dataservice.js
+++ js/factories/dataservice.js
@@ -5,38 +5,19 @@
 
     function DataService() {
         var dataObj = {
-            processFile: processFile()
+            resumeActive: false,
+            activate: activate,
+            dataParse: dataParse
             //TODO: Data Parse
         };
         return dataObj;
-    }
 
-    var fileInput = $('#files');
-    var uploadButton = $('#upload');
-
-    uploadButton.on('click', function() {
-        if(!window.FileReader) {
-            alert('Your browser is not supported')
+        function activate(state) {
+            dataObj.resumeActive = state
         }
-        var input = fileInput.get(0);
 
-        // Create a reader object
-        var reader = new FileReader();
-        if(input.files.length) {
-            var textFile = input.files[0];
-            reader.readAsText(textFile);
-            $(reader).on('load', processFile);
-        } else {
-            alert('Please upload a file before continuing')
+        function dataParse(file) {
+            return file.split("\n");
         }
-    });
-
-    function processFile(e) {
-        var file = e.target.result,
-            results;
-        if(file && file.length) {
-            results = file.split("\n");
-        }
-        return results;
     }
 })();
